65ce7763c2f6489c2192e556baeedd8d81b1bff5
Douglas <douglasd3v.contato@gmail.com>
2024-11-27 22:55:52 -0300

    Criação da página de cadastro e da função de conexão do banco de dados

---
+++ app/functions/database.php
@@ -0,0 +1,22 @@
+<?php
+
+function connect()
+{
+  $pdo = new \PDO("mysql:host=localhost;dbname=blog;charset=utf8", "root", "root");
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+
+  return $pdo;
+}
+function create()
+{
+}
+function update()
+{
+}
+function find()
+{
+}
+function delete()
+{
+}
